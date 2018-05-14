a9d841e7fb93fabcc1bf491ed4c65c2e8e359484
hui3694 <1054541133@qq.com>
2018-05-15 02:24:36 +0800

    11

--- CMS/Areas/Admin/Controllers/MainController.cs
+++ CMS/Areas/Admin/Controllers/MainController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CMS.Areas.Admin.Models;
 
 namespace CMS.Areas.Admin.Controllers
 {
     public class MainController : Controller
     {
         // GET: Admin/Main
+
         public ActionResult Index()
         {
+            //进入后台
+            return View();
+        }
+
+        public ActionResult Login()
+        {
             return View("Login");
+        }
+
+        public ActionResult DoLogin(Models.admin admin)
+        {
+            //List<Models.admin> adminList = new Models.adminDAL().admin.ToList();
+            adminDAL adminDal = new adminDAL();
+
+            List<admin> list = adminDal.admin
+                .Where<admin>(a => a.UserName == admin.UserName && a.PassWord == admin.PassWord).ToList();
+
+            if (list.Count > 0)
+            {
+                //登陆成功
+                return RedirectToAction("Index", "Home",new {area=""});
+            }
+            else
+            {
+                //登陆失败
+                return View("Login");
+            }
         }
     }
 }
---
+++ CMS/Areas/Admin/Models/admin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMS.Areas.Admin.Models
+{
+    public class admin
+    {
+        [Key]
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+    }
+
+    public class adminDAL : DbContext
+    {
+        public DbSet<admin> admin { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<admin>().ToTable("tb_admin");
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
